e7113f49ea1d628c509e69acd73c10778d0c0d37
Matheus Oliveira <matheusfilho98.ms@gmail.com>
2021-10-05 23:07:07 -0300

    fix: bug fix with process

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -1,9 +1,7 @@
-export const ENV = process.env;
+export const GITHUB_API = process.env.GITHUB_API_URL;
 
-export const GITHUB_API = ENV.GITHUB_API_URL;
+export const IS_PROD = process.env.NODE_ENV === 'production';
 
-export const IS_PROD = ENV.NODE_ENV === 'production';
+export const IS_DEV = process.env.NODE_ENV === 'development';
 
-export const IS_DEV = ENV.NODE_ENV === 'development';
-
-export const IS_TEST = ENV.NODE_ENV === 'test';
+export const IS_TEST = process.env.NODE_ENV === 'test';
